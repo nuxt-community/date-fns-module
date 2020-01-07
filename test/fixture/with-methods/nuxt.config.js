f6517cf2976757a1305485649f6023ab6aa1c095
Matthias Ringwald <matthias_ringwald@hotmail.com>
2020-01-07 14:26:10 +0100

    feat: make injected date-fns treeshakeable (#25)
    * feat: make injected date-fns treeshakeable
    
    * fix: code styling and package.json
    
    * fix: indent in plugin.js
    
    * fix: added methods to options, fixed spaces before parenthesis

---
+++ test/fixture/with-methods/nuxt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rootDir: __dirname,
+  render: {
+    resourceHints: false
+  },
+  buildModules: [
+    { handler: require('../../../') }
+  ],
+  dateFns: {
+    locales: ['es', 'ru', 'pt-BR'],
+    defaultLocale: 'en-US',
+    format: 'yyyy-MM-dd',
+    methods: ['format', 'parseISO']
+  }
+}
