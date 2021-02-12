6ce9a21d6a29a59eea23257d6cc67656caf102d8
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-02-12 17:33:18 +0100

    feat: add fallback locale

---
+++ test/fixture/fallback-locale/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ $dateFns.format('1995-12-25', 'EEEE', { locale: 'unsupportedLocale' }) }}
+    <p v-text="str" />
+    {{ $dateFns.format('1995-12-25', 'yyyy') }}
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData (ctx) {
+    return {
+      str: ctx.app.$dateFns.format('1995-12-25', 'MMMM')
+    }
+  }
+}
+</script>
